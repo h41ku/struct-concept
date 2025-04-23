63160ac051f3b51298a8002557d2f48877958c4e
h41ku <h41ku.c0de@ya.ru>
2025-04-23 17:24:05 +0300

    Add symbol `isComputed`

--- src/symbols.js
+++ src/symbols.js
@@ -1,4 +1,5 @@
 export const rawDef = Symbol('rawDef')
 export const isStruct = Symbol('isStruct')
 export const isVector = Symbol('isVector')
+export const isComputed = Symbol('isComputed')
 export const sizeOf = Symbol('sizeOf')
